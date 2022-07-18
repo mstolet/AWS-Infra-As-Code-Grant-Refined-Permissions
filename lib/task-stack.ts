cdd298f66f4e1e44ac7606e3aa155c399fbed181
mstoelt <mstoletb@gmail.com>
2022-07-18 10:46:57 -0300

    everything working. starting to fix the access to everything problem

--- lib/task-stack.ts
+++ lib/task-stack.ts
@@ -17,9 +17,9 @@ export class TaskStack extends Stack {
         });
     
         //create user
-        const IAMUser = new iam.CfnUser(this, 'IAMUser', {
+        const IAMUser = new iam.CfnUser(this, 'IAMUser3', {
           path: "/",
-          userName: "SensitiveReader2",
+          userName: "SensitiveReader3",
           managedPolicyArns: [
               "arn:aws:iam::aws:policy/AmazonS3FullAccess",
               "arn:aws:iam::aws:policy/AmazonAthenaFullAccess",
@@ -39,10 +39,6 @@ export class TaskStack extends Stack {
           name: "AthenaWorkGroup45",
           state: "ENABLED",
           workGroupConfiguration: {
-              //bytesScannedCutoffPerQuery: 1099511627776000,
-              //enforceWorkGroupConfiguration: false,
-              //publishCloudWatchMetricsEnabled: true,
-              //requesterPaysEnabled: false,
               resultConfiguration: {
                   outputLocation: `s3://${s3Bucket.bucketArn}/`
                }
@@ -55,6 +51,37 @@ export class TaskStack extends Stack {
         databaseInput: {
           name: "database_cdk_test"
       }});
+
+      const cfnPermissions = new lakeformation.CfnPermissions(this, 'MyCfnPermissions', {
+        dataLakePrincipal: {
+          dataLakePrincipalIdentifier: IAMUser.attrArn,
+        },
+        resource: {
+          databaseResource: {
+            catalogId: Stack.of(this).account,
+            name: "database_cdk_test",
+          },
+          dataLocationResource: {
+            catalogId: Stack.of(this).account,
+            s3Resource: `s3://${s3Bucket.bucketArn}/`,
+          },
+          tableResource: {
+            catalogId: Stack.of(this).account,
+            databaseName: 'databaseName',
+            name: 'name',
+            tableWildcard: { },
+          },
+          tableWithColumnsResource: {
+            catalogId: Stack.of(this).account,
+            columnNames: ['1','2','3','4','5','6','7','8','9','10','11'],
+            columnWildcard: {
+              excludedColumnNames: ['6','7','8','9','10','11'],
+            },
+            databaseName: "database_cdk_test",
+            name: 'name',
+          },
+        },
+      });
 
         //creating table
         const GlueTable = new glue.CfnTable(this, 'GlueTable', {
@@ -109,51 +136,187 @@ export class TaskStack extends Stack {
                       }
                   ],
                   location: s3Bucket.bucketArn,
-                  /*inputFormat: "org.apache.hadoop.mapred.TextInputFormat",
-                  outputFormat: "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat",
-                  compressed: false,
-                  numberOfBuckets: -1,
-                  serdeInfo: {
-                      serializationLibrary: "org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe",
-                  },
-                  storedAsSubDirectories: false*/
               },
-              //retention: 0,
               name: "table_cdk_test"
               //Refer to resource
           }
       });
 
-      const cfnPermissions = new lakeformation.CfnPermissions(this, 'MyCfnPermissions', {
-        dataLakePrincipal: {
-          dataLakePrincipalIdentifier: IAMUser.attrArn,
-        },
-        resource: {
-          databaseResource: {
-            catalogId: Stack.of(this).account,
-            name: "database_cdk_test",
-          },
-          dataLocationResource: {
-            catalogId: Stack.of(this).account,
-            s3Resource: `s3://${s3Bucket.bucketArn}/`,
-          },
-          tableResource: {
-            catalogId: Stack.of(this).account,
-            databaseName: 'databaseName',
-            name: 'name',
-            tableWildcard: { },
-          },
-          tableWithColumnsResource: {
-            catalogId: Stack.of(this).account,
-            columnNames: ['1','2','3','4','5','6','7','8','9','10','11'],
-            columnWildcard: {
-              excludedColumnNames: ['6','7','8','9','10','11'],
+
+      /*const GlueTable = new glue.CfnTable(this, 'GlueTable', {
+        catalogId: Stack.of(this).account,
+        databaseName: "database_cdk_test",
+        tableInput: {
+            owner: "hadoop",
+            tableType: "EXTERNAL_TABLE",
+            parameters: {
+                external: "TRUE",
+                has_encrypted_data: "false",
+                //transient_lastDdlTime: "1656951835"
             },
-            databaseName: "database_cdk_test",
-            name: 'name',
-          },
+            storageDescriptor: {
+                columns: [
+                    {
+                        name: "1",
+                        type: "string"
+                    },
+                    {
+                        name: "2",
+                        type: "string"
+                    },
+                    {
+                        name: "3",
+                        type: "string"
+                    },
+                    {
+                        name: "4",
+                        type: "string"
+                    },
+                    {
+                        name: "5",
+                        type: "string"
+                    },
+                    {
+                        name: "6",
+                        type: "string"
+                    },
+                    {
+                        name: "7",
+                        type: "string"
+                    },
+                    {
+                        name: "8",
+                        type: "string"
+                    },
+                    {
+                        name: "9",
+                        type: "string"
+                    },
+                    {
+                        name: "10",
+                        type: "string"
+                    },
+                    {
+                        name: "11",
+                        type: "string"
+                    }
+                ],
+                location: s3Bucket.bucketArn,
+                inputFormat: "org.apache.hadoop.mapred.TextInputFormat",
+                outputFormat: "org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat",
+                compressed: false,
+                numberOfBuckets: -1,
+                storedAsSubDirectories: false
+            },
+            retention: 0,
+            name: "tableeee"
+        }
+    });
+
+    const cfnTable = new glue.CfnTable(this, 'MyCfnTable', {
+      catalogId: Stack.of(this).account,
+      databaseName: "database_cdk_test",
+      tableInput: {
+        name: 'name',
+        owner: Stack.of(this).account,
+        //parameters: parameters,
+        //partitionKeys: [{
+          //name: 'name',
+    
+          // the properties below are optional
+          //comment: 'comment',
+          //type: 'type',
         },
-      });
+        //retention: 0,
+      storageDescriptor: {
+        bucketColumns: ['1','2','3','4','5','6','7','8','9','10','11'],
+        columns: [
+          {
+            name: "1",
+            type: "string"
+          },
+          {
+            name: "2",
+            type: "string"
+          },
+          {
+            name: "3",
+            type: "string"
+          },
+          {
+            name: "4",
+            type: "string"
+          },
+          {
+            name: "5",
+            type: "string"
+          },
+          {
+            name: "6",
+            type: "string"
+          },
+          {
+            name: "7",
+            type: "string"
+          },
+          {
+            name: "8",
+            type: "string"
+          },
+          {
+            name: "9",
+            type: "string"
+          },
+          {
+            name: "10",
+            type: "string"
+          },
+          {
+            name: "11",
+            type: "string"
+          }
+          ],
+        compressed: false,
+          //inputFormat: 'inputFormat',
+        location: s3Bucket.bucketArn,
+          //numberOfBuckets: 1,
+          //outputFormat: 'outputFormat',
+          //parameters: parameters,
+          /*schemaReference: {
+            schemaId: {
+              registryName: 'registryName',
+              schemaArn: 'schemaArn',
+              schemaName: 'schemaName',
+            },
+            schemaVersionId: 'schemaVersionId',
+            schemaVersionNumber: 123,
+          },
+          serdeInfo: {
+            name: 'name',
+            parameters: parameters,
+            serializationLibrary: 'serializationLibrary',
+          },
+          skewedInfo: {
+            skewedColumnNames: ['skewedColumnNames'],
+            skewedColumnValueLocationMaps: skewedColumnValueLocationMaps,
+            skewedColumnValues: ['skewedColumnValues'],
+          },
+          sortColumns: [{
+            column: 'column',
+            sortOrder: 123,
+          }],
+          storedAsSubDirectories: false,
+        },
+        tableType: 'tableType',
+        targetTable: {
+          catalogId: 'catalogId',
+          databaseName: 'databaseName',
+          name: 'name',
+        },
+        viewExpandedText: 'viewExpandedText',
+        viewOriginalText: 'viewOriginalText',
+      },
+    });*/
 
         //creating query
         const cfnNamedQuery = new athena.CfnNamedQuery(this, 'MyCfnNamedQuery', {
